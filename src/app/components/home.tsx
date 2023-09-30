f33da3ff921826d35fc65981518cece5ed630522
clareysean <seanpclarey@gmail.com>
2023-09-30 18:25:53 -0400

    add a page

---
+++ src/app/components/home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+export default function Home({}: Props) {
+  return <div>home</div>;
+}
+
+// call an api method
+
+// POST https:
+
+//www.strava.com/oauth/token with its client ID and client secret to exchange the authorization code for a refresh token and short-lived access token.
---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "../components/home";
+
+type Props = {};
+
+export default function page({}: Props) {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
